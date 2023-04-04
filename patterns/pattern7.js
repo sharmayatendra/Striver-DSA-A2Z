3037e2356d50d6674c4fe85ef14ce0e9abc657a1
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-04-04 13:03:12 +0530

    added pattern7

---
+++ patterns/pattern7.js
@@ -0,0 +1,38 @@
+/*
+o/p:      *
+        * * *
+      * * * * * 
+    * * * * * * *
+  * * * * * * * * *
+*/
+
+// logic: [space, stars, space]
+// n = 5,  0    , 9   , 0
+//  n = 4,  1    , 7   , 1
+//  n = 3,  2    , 5   , 2
+//  n = 2,  3    , 3   , 3
+//  n = 1,  4    , 1   , 4
+
+function pattern7(n) {
+  let str = "";
+  for (let i = 0; i <= n; i++) {
+    // space:
+    for (let j = 1; j <= n - i; j++) {
+      str += " ";
+    }
+
+    // stars:
+    for (let j = 1; j <= 2 * i - 1; j++) {
+      str += "*";
+    }
+
+    // space:
+    for (let j = 1; j <= n - i; j++) {
+      str += " ";
+    }
+    str += "\n";
+  }
+  return str;
+}
+
+console.log(pattern7(5));
